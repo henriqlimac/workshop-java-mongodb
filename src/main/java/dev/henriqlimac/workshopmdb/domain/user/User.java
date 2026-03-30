8f780ad1288b0797e69881c4577814a07d445ae7
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-30 01:57:47 -0300

    fix: removed 'id' setter and constructor

--- src/main/java/dev/henriqlimac/workshopmdb/domain/user/User.java
+++ src/main/java/dev/henriqlimac/workshopmdb/domain/user/User.java
@@ -20,18 +20,13 @@ public class User implements Serializable {
     public User() {
     }
 
-    public User(String id, String name, String email) {
-        this.id = id;
+    public User(String name, String email) {
         this.name = name;
         this.email = email;
     }
 
     public String getId() {
         return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getName() {
