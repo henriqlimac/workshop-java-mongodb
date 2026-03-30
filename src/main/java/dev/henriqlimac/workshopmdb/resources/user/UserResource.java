1af70233047fce5e8508b847fd97f25a6da34fe5
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-30 02:13:55 -0300

    feat: added 'UserDTO' layer to findAll

--- src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
+++ src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
@@ -1,6 +1,7 @@
 package dev.henriqlimac.workshopmdb.resources.user;
 
 import dev.henriqlimac.workshopmdb.domain.user.User;
+import dev.henriqlimac.workshopmdb.dto.user.UserDTO;
 import dev.henriqlimac.workshopmdb.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
-        List<User> users = service.findAll();
+    public ResponseEntity<List<UserDTO>> findAll() {
+        List<User> list = service.findAll();
+        List<UserDTO> users = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(users);
     }
