0d9da0f5f2978c59cf758abf7bbc3e4e3487f997
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-30 02:05:10 -0300

    feat: initial data instantiation

---
+++ src/main/java/dev/henriqlimac/workshopmdb/config/Instantiation.java
@@ -0,0 +1,26 @@
+package dev.henriqlimac.workshopmdb.config;
+
+import dev.henriqlimac.workshopmdb.domain.user.User;
+import dev.henriqlimac.workshopmdb.respositories.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User("Maria Brown", "maria@gmail.com");
+        User alex = new User("Alex Yellow", "alex@gmail.com");
+        User bob = new User("Bob Red", "bob@gmail.com");
+
+        userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
