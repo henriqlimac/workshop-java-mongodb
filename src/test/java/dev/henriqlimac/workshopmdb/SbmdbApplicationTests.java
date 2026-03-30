fe61a2f74504f11366680817b240b0d34840a268
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-30 00:57:07 -0300

    first: project created

---
+++ src/test/java/dev/henriqlimac/workshopmdb/SbmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.henriqlimac.workshopmdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
