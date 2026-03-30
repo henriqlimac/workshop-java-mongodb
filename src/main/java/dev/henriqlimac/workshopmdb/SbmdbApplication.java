2523ea983e9581d9854cba259a9009dfe3f44fbe
henriqlimac <h.cardosinho.lima@gmail.com>
2026-03-30 00:57:14 -0300

    first: project created

--- src/main/java/dev/henriqlimac/workshopmdb/SbmdbApplication.java
+++ src/main/java/dev/henriqlimac/workshopmdb/SbmdbApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SbmdbApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbmdbApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SbmdbApplication.class, args);
+    }
 
 }
