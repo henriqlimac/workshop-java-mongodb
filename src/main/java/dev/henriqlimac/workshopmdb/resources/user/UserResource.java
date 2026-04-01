5e4bebb3c8544a89d0fe35c0606120d043792d9d
henriqlimac <h.cardosinho.lima@gmail.com>
2026-04-01 02:06:36 -0300

    feat: 'User' insert method

--- src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
+++ src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
@@ -5,11 +5,10 @@ import dev.henriqlimac.workshopmdb.dto.user.UserDTO;
 import dev.henriqlimac.workshopmdb.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -33,5 +32,14 @@ public class UserResource {
         UserDTO user = new UserDTO(service.findById(id));
 
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO dto) {
+        User user = service.fromDTO(dto);
+        user = service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/dev/henriqlimac/workshopmdb/services/user/UserService.java
+++ src/main/java/dev/henriqlimac/workshopmdb/services/user/UserService.java
@@ -1,6 +1,7 @@
 package dev.henriqlimac.workshopmdb.services.user;
 
 import dev.henriqlimac.workshopmdb.domain.user.User;
+import dev.henriqlimac.workshopmdb.dto.user.UserDTO;
 import dev.henriqlimac.workshopmdb.respositories.user.UserRepository;
 import dev.henriqlimac.workshopmdb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,10 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    public User fromDTO(UserDTO dto) {
+        return new User(dto.getName(), dto.getEmail());
+    }
+
     public List<User> findAll() {
         return repository.findAll();
     }
@@ -26,5 +31,9 @@ public class UserService {
         }
 
         return user;
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
     }
 }
