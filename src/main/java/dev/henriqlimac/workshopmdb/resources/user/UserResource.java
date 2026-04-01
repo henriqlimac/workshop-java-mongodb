82253ac266cadda31ac569a94eb60a645ad8e2cb
henriqlimac <h.cardosinho.lima@gmail.com>
2026-04-01 01:54:55 -0300

    feat: 'User' findById method

--- src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
+++ src/main/java/dev/henriqlimac/workshopmdb/resources/user/UserResource.java
@@ -6,10 +6,10 @@ import dev.henriqlimac.workshopmdb.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,5 +26,12 @@ public class UserResource {
         List<UserDTO> users = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO user = new UserDTO(service.findById(id));
+
+        return ResponseEntity.ok().body(user);
     }
 }
--- src/main/java/dev/henriqlimac/workshopmdb/services/user/UserService.java
+++ src/main/java/dev/henriqlimac/workshopmdb/services/user/UserService.java
@@ -2,6 +2,7 @@ package dev.henriqlimac.workshopmdb.services.user;
 
 import dev.henriqlimac.workshopmdb.domain.user.User;
 import dev.henriqlimac.workshopmdb.respositories.user.UserRepository;
+import dev.henriqlimac.workshopmdb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,15 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repository.findById(id).orElse(null);
+
+        if (user == null) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+
+        return user;
     }
 }
